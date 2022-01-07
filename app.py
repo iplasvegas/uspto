f91d5aa461dee963b95709743d25e10127de6f27
Kevin Jay <kevinjay566@gmail.com>
2022-01-06 19:12:19 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+# Connect webdriver to chrome
+browser = webdriver.Chrome(ChromeDriverManager().install())  #executable_path='/Users/kevinjay/projects/gecko/chromedriver.exe')
+browser.get('https://tsdr.uspto.gov/')
+
+serial_input = browser.find_element(By.ID, 'searchNumber')
+serial_input.send_keys('85931937' + Keys.RETURN)
+
