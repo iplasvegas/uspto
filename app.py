4faa10d2a1a69b3429ef60983cae44e862589a9b
Kevin Jay <kevinjay566@gmail.com>
2022-01-13 22:43:53 -0800

    Found Registration Date

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 # Connect webdriver to chrome
 browser = webdriver.Chrome(ChromeDriverManager().install())  #executable_path='/Users/kevinjay/projects/gecko/chromedriver.exe')
 browser.get('https://tsdr.uspto.gov/')
@@ -11,7 +15,4 @@ browser.get('https://tsdr.uspto.gov/')
 serial_input = browser.find_element(By.ID, 'searchNumber')
 serial_input.send_keys('85931937' + Keys.RETURN)
 
-values = browser.find_elements(By.CLASS_NAME, 'value')
-print(values)
-
-# Hi Jake, make a branch and delete this
+print(WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='key' and text()='Registration Date:']//following-sibling::div[1]"))).get_attribute("innerHTML"))
