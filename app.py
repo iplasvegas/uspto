6a0bd1ef87202b45a58748aed2ce02864703583d
Kevin Jay <kevinjay566@gmail.com>
2022-01-14 12:19:51 -0800

    creating csv

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+import pandas as pd
 # Connect webdriver to chrome
 browser = webdriver.Chrome(ChromeDriverManager().install())  #executable_path='/Users/kevinjay/projects/gecko/chromedriver.exe')
 browser.get('https://tsdr.uspto.gov/')
@@ -15,5 +16,18 @@ browser.get('https://tsdr.uspto.gov/')
 serial_input = browser.find_element(By.ID, 'searchNumber')
 serial_input.send_keys('85931937' + Keys.RETURN)
 
-print(WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='key' and text()='Registration Date:']//following-sibling::div[1]"))).get_attribute("innerHTML"))
+registration_date = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='key' and text()='Registration Date:']//following-sibling::div[1]"))).get_attribute("innerHTML")
+application_filing_date = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='key' and text()='Application Filing Date:']//following-sibling::div[1]"))).get_attribute("innerHTML")
+us_serial_number = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='key' and text()='US Serial Number:']//following-sibling::div[1]"))).get_attribute("innerHTML")
 
+data = {
+    'registration_date': registration_date.strip(),
+    'application_filing_date': application_filing_date.strip(),
+    'us_serial_number': us_serial_number.strip(),
+}
+
+# Convert to pd.df
+data_for_csv = pd.DataFrame(data, index=[0])
+
+# Output the csv to the path
+data_for_csv.to_csv('outputs.csv', index=False)
