28ca89e486ea26a3b3f6197f58886ddbfd53e761
jacobtsmith <91043931+jacobtsmith@users.noreply.github.com>
2022-01-20 11:06:56 -0800

    Optimize imports based on PyCharm suggestions

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import pandas as pd
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
